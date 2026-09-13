9e3609f9dd841a095086e95404b0c40c49cacd36
bolohamwich <bolohamwich@gmail.com>
2026-09-13 16:57:52 +0300

    Indentation error

--- src/ticker_icon.py
+++ src/ticker_icon.py
@@ -192,7 +192,7 @@ class TickerIcon:
         """Background loop that periodically refreshes market data for all tickers."""
         while self.running:
             if self.paused.is_set():
-self._wait_while_paused()
+                self._wait_while_paused()
                 continue
 
             with self.lock:
